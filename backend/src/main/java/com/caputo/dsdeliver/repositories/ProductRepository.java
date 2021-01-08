9d0ef06cfbd048d853def3da4205795f76274e39
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-01-08 18:29:46 -0300

    GET /products

---
+++ backend/src/main/java/com/caputo/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.caputo.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.caputo.dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	List<Product> findAllByOrderByNameAsc();
+}
