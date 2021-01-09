f30dc7c585ca17af7c08e2d98de92b56514da4b8
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-01-09 17:19:45 -0300

    GET /orders

---
+++ backend/src/main/java/com/caputo/dsdeliver/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.caputo.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.caputo.dsdeliver.dto.OrderDTO;
+import com.caputo.dsdeliver.entities.Order;
+import com.caputo.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();	
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
